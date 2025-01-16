894b3cae13a9b1655d14523a80df0019d6dc5b26
IBoTuS <romanmiakotaa@gmail.com>
2025-01-17 01:29:41 +0200

    V0.5 - Удалил ненужный массив, который сортирует ресурсы при открытии

--- src/main/java/dev/cakestudio/cakeenderchestfilter/listener/InventoryClickListener.java
+++ src/main/java/dev/cakestudio/cakeenderchestfilter/listener/InventoryClickListener.java
@@ -3,7 +3,6 @@ package dev.cakestudio.cakeenderchestfilter.listener;
 import dev.cakestudio.cakeenderchestfilter.configuration.Config;
 import dev.cakestudio.cakeenderchestfilter.utils.HexColor;
 import org.bukkit.Sound;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,13 +22,13 @@ public class InventoryClickListener implements Listener {
         if (!player.hasPermission("cakeenderchestfilter.bypass")) {
             if (event.getInventory().getType().equals(InventoryType.ENDER_CHEST)) {
                 if (event.getCurrentItem() != null) {
-                    boolean FilterItem = Config.getConfig().getStringList("settings.items-filter").stream()
+                    boolean items = Config.getConfig().getStringList("settings.items-filter").stream()
                             .anyMatch(item -> item.equals(event
                                     .getCurrentItem()
                                     .getType()
                                     .toString()));
 
-                    if (FilterItem && event.getAction().equals(InventoryAction.MOVE_TO_OTHER_INVENTORY)) {
+                    if (items && event.getAction().equals(InventoryAction.MOVE_TO_OTHER_INVENTORY)) {
                         event.setCancelled(true);
 
                         String sound = Config.getConfig().getString("settings.sound");
@@ -47,36 +46,33 @@ public class InventoryClickListener implements Listener {
     @EventHandler
     public void onInventoryCloseEvent(@NotNull InventoryCloseEvent event) {
         if (event.getInventory().getType() == InventoryType.ENDER_CHEST) {
-            List<HumanEntity> viewers = event.getInventory().getViewers();
-            if (!viewers.isEmpty()) {
-                Player player = (Player) viewers.get(0);
-                if (!player.hasPermission("cakeenderchestfilter.bypass")) {
-                    ItemStack[] enderChestItems = event.getInventory().getContents();
+            Player player = (Player) event.getPlayer();
+            if (!player.hasPermission("cakeenderchestfilter.bypass")) {
+                ItemStack[] items = event.getInventory().getContents();
+                List<ItemStack> give = new ArrayList<>();
 
-                    List<ItemStack> toGive = Arrays.stream(enderChestItems)
-                            .filter(item -> item != null && Config.getConfig().getStringList("settings.items-filter").contains(item.getType().toString()))
-                            .toList();
-
-                    event.getInventory().setContents(Arrays.stream(enderChestItems)
-                            .filter(item -> item != null && !Config.getConfig().getStringList("settings.items-filter").contains(item.getType().toString())).toArray(ItemStack[]::new));
-
-                    toGive.forEach(item -> {
-                        HashMap<Integer, ItemStack> notAdded = player.getInventory().addItem(item);
-                        if (!notAdded.isEmpty()) {
-                            player.getWorld().dropItem(player.getLocation(), notAdded.get(0));
-                        }
-                    });
-
-                    if (!toGive.isEmpty()) {
-                        player.closeInventory();
-
-                        String sound = Config.getConfig().getString("settings.sound");
-                        Sound sound1 = Sound.valueOf(sound);
-                        player.playSound(player.getLocation(), sound1, 1.0f, 1.0f);
-
-                        String message = Config.getConfig().getString("messages.player");
-                        player.sendMessage(HexColor.color(message));
+                for (int i = 0; i < items.length; i++) {
+                    ItemStack item = items[i];
+                    if (item != null && Config.getConfig().getStringList("settings.items-filter").contains(item.getType().toString())) {
+                        give.add(item);
+                        event.getInventory().setItem(i, null);
                     }
+                }
+
+                give.forEach(item -> {
+                    HashMap<Integer, ItemStack> notAdded = player.getInventory().addItem(item);
+                    if (!notAdded.isEmpty()) {
+                        player.getWorld().dropItem(player.getLocation(), notAdded.values().iterator().next());
+                    }
+                });
+
+                if (!give.isEmpty()) {
+                    String sound = Config.getConfig().getString("settings.sound");
+                    Sound sound1 = Sound.valueOf(sound);
+                    player.playSound(player.getLocation(), sound1, 1.0f, 1.0f);
+
+                    String message = Config.getConfig().getString("messages.player");
+                    player.sendMessage(HexColor.color(message));
                 }
             }
         }
@@ -85,43 +81,39 @@ public class InventoryClickListener implements Listener {
     @EventHandler
     public void onInventoryOpenEvent(@NotNull InventoryOpenEvent event) {
         if (event.getInventory().getType() == InventoryType.ENDER_CHEST) {
-            List<HumanEntity> viewers = event.getInventory().getViewers();
-            if (!viewers.isEmpty()) {
-                Player player = (Player) viewers.get(0);
-                if (!player.hasPermission("cakeenderchestfilter.bypass")) {
-                    ItemStack[] enderChestItems = event.getInventory().getContents();
+            Player player = (Player) event.getPlayer();
+            if (!player.hasPermission("cakeenderchestfilter.bypass")) {
+                ItemStack[] items = event.getInventory().getContents();
+                List<ItemStack> give = new ArrayList<>();
 
-                    List<ItemStack> enderChestArray = Arrays.stream(enderChestItems)
-                            .filter(item -> item != null && !Config.getConfig().getStringList("settings.items-filter").contains(item.getType().toString()))
-                            .toList();
-
-                    List<ItemStack> toGive = Arrays.stream(enderChestItems)
-                            .filter(item -> item != null && Config.getConfig().getStringList("settings.items-filter").contains(item.getType().toString()))
-                            .toList();
-
-                    event.getInventory().setContents(enderChestArray.toArray(new ItemStack[0]));
-
-                    toGive.forEach(item -> {
-                        player.getInventory().addItem(item);
-
-                        String sound = Config.getConfig().getString("settings.sound");
-                        Sound sound1 = Sound.valueOf(sound);
-                        player.playSound(player.getLocation(), sound1, 1.0f, 1.0f);
-
-                        String message = Config.getConfig().getString("messages.player");
-                        player.sendMessage(HexColor.color(message));
-                    });
-
-                    if (!toGive.isEmpty()) {
-                        player.closeInventory();
-
-                        String sound = Config.getConfig().getString("settings.sound");
-                        Sound sound1 = Sound.valueOf(sound);
-                        player.playSound(player.getLocation(), sound1, 1.0f, 1.0f);
-
-                        String message = Config.getConfig().getString("messages.player");
-                        player.sendMessage(HexColor.color(message));
+                for (int i = 0; i < items.length; i++) {
+                    ItemStack item = items[i];
+                    if (item != null && Config.getConfig().getStringList("settings.items-filter").contains(item.getType().toString())) {
+                        give.add(item);
+                        event.getInventory().setItem(i, null);
                     }
+                }
+
+                give.forEach(item -> {
+                    player.getInventory().addItem(item);
+
+                    String sound = Config.getConfig().getString("settings.sound");
+                    Sound sound1 = Sound.valueOf(sound);
+                    player.playSound(player.getLocation(), sound1, 1.0f, 1.0f);
+
+                    String message = Config.getConfig().getString("messages.player");
+                    player.sendMessage(HexColor.color(message));
+                });
+
+                if (!give.isEmpty()) {
+                    player.closeInventory();
+
+                    String sound = Config.getConfig().getString("settings.sound");
+                    Sound sound1 = Sound.valueOf(sound);
+                    player.playSound(player.getLocation(), sound1, 1.0f, 1.0f);
+
+                    String message = Config.getConfig().getString("messages.player");
+                    player.sendMessage(HexColor.color(message));
                 }
             }
         }
@@ -133,11 +125,11 @@ public class InventoryClickListener implements Listener {
         Inventory openInventory = player.getOpenInventory().getTopInventory();
 
         if (openInventory.getType().equals(InventoryType.ENDER_CHEST)) {
-            List<ItemStack> toDrop = Arrays.stream(openInventory.getContents())
+            List<ItemStack> drop = Arrays.stream(openInventory.getContents())
                     .filter(item -> item != null && Config.getConfig().getStringList("settings.items-filter").contains(item.getType().toString()))
                     .toList();
 
-            toDrop.forEach(item -> {
+            drop.forEach(item -> {
                 openInventory.remove(item);
                 player.getWorld().dropItemNaturally(player.getLocation(), item);
             });
